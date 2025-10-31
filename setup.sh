1f06a5390388885ff09ddb1f7a6a42c3422e72f5
jeanr <jeanr@met.no>
2025-10-31 18:18:05 +0100

    Set up bash-git-prompt

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ./setup_command_line
+bash setup_command_line.sh
+cd ..
---
+++ setup_command_line/setup_command_line.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# setup https://github.com/magicmonty/bash-git-prompt
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+
+cat << EOF >> "$HOME/.bashrc"
+
+# set up gitprompt
+if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=0
+    GIT_PROMPT_THEME=Single_line_Dark  # this one is nice
+    source $HOME/.bash-git-prompt/gitprompt.sh
+fi
+
+EOF
