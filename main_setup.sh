1d5ae92cfc5644da9f462ebbcd961144920edc05
jeanr <jeanr@met.no>
2026-01-13 18:27:07 +0100

    Add main entry point that does not assume specific location

---
+++ main_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -i
+
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+# setxkbmap no
+
+source "$HOME/.bashrc"
+cd ./setup_standard_libraries
+bash setup_standard_libraries.sh
+cd "$HOME/Desktop/Git/setups"
+
+source "$HOME/.bashrc"
+cd ./setup_command_line
+bash setup_command_line.sh
+cd "$HOME/Desktop/Git/setups"
+
+source "$HOME/.bashrc"
+cd ./setup_tmux
+bash setup_tmux.sh
+cd "$HOME/Desktop/Git/setups"
+
+source "$HOME/.bashrc"
+cd ./setup_rust
+bash setup_rust.sh
+cd "$HOME/Desktop/Git/setups"
+
+source "$HOME/.bashrc"
+cd ./setup_helix
+bash setup_helix.sh
+cd "$HOME/Desktop/Git/setups"
+
+source "$HOME/.bashrc"
+cd ./setup_nvm_npm
+bash setup_nvm_npm.sh
+cd "$HOME/Desktop/Git/setups"
+npm install -g @github/copilot
