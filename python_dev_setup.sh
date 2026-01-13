f21ce6e17e5a8002faaaf51bb28927a10284cf7f
jeanr <jeanr@met.no>
2026-01-13 18:23:24 +0100

    WIP

--- python_dev_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -i
-
-# TODO:
-# mamba
-# "dev" standard env
-# there, standard packages I use "all the time"
-# add to bash history the command to enable it all
-# add hj script for helix
-# add lsp for helix
