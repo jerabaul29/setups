e812f669c2f3401285b7d596dd054dcec1332279
jeanr <jeanr@met.no>
2026-01-22 09:15:42 +0100

    Note podman

--- setup_standard_libraries/setup_standard_libraries.sh
+++ setup_standard_libraries/setup_standard_libraries.sh
@@ -5,3 +5,4 @@ sudo apt upgrade -y
 sudo apt install build-essential libc6-dev -y
 sudo apt install libutil-dev librt-dev libpthread-stubs0-dev libm-dev libdl-dev -y
 sudo apt install lld -y
+# TODO: podman
