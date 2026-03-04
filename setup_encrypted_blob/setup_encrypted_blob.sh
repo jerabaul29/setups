d574d0da5ecce90f09127c5e76b5064562c9b249
jeanr <jeanr@met.no>
2026-03-04 20:39:15 +0100

    WIP

--- setup_encrypted_blob/setup_encrypted_blob.sh
+++ setup_encrypted_blob/setup_encrypted_blob.sh
@@ -6,6 +6,9 @@ PATH_TO_BLOB="$HOME/Desktop/Git/setups/setup_encrypted_blob"
 BLOB_BASENAME="blob"
 ENTRY_POINT_BLOB="main.sh"
 
+rm -rf "${PATH_TO_BLOB}/${BLOB_BASENAME}.zip"
+rm -rf "${PATH_TO_BLOB}/extracted"
+
 gpg --output "${PATH_TO_BLOB}/${BLOB_BASENAME}.zip" --decrypt "${PATH_TO_BLOB}/${BLOB_BASENAME}.zip.gpg"
 unzip "${PATH_TO_BLOB}/${BLOB_BASENAME}.zip" -d "${PATH_TO_BLOB}/extracted/"
 
