45b8e3cbb64a9440f4533e83405a877b389cb7ca
jeanr <jeanr@met.no>
2026-08-20 16:34:23 +0200

    Install shellcheck

--- setup_bash/setup_bash.sh
+++ setup_bash/setup_bash.sh
@@ -4,3 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 npm i -g bash-language-server
+
+# add shellcheck linter for bash
+sudo apt install shellcheck
